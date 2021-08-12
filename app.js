3cabb9034fec616a67e4be527c474cd8ee83a67b
Carlos-SSH <carlos.anezeh@me.com>
2021-08-11 23:05:58 -0500

    Refactorized displayBooks function.

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 /* eslint no-use-before-define:["error",{"functions":false}] */
 class Library {
-  constructor {
+  constructor() {
     this.library = JSON.parse(localStorage.getItem('library') || '[]');
     this.id = 0;
   }
 
   // eslint-disable-next-line no-unused-vars
-  function addBook() {
+  addBook() {
     const book = {};
     this.id += 1;
     book.id = this.id;
     this.library.push(book);
   }
 
-  function removeBook(id) {
+  removeBook(id) {
     this.library = library.filter((book) => book.id !== id);
   }
 
 
-  function saveLibrary() {
+  saveLibrary() {
     localStorage.setItem('library', JSON.stringify(library));
   }
 }
@@ -27,15 +27,15 @@ const myLibrary = new Library;
 
 function addBook() {
   const book = {};
-  book.author = document.getElementById('Author').value = '';
-  book.title = document.getElementById('Title').value = '';
+  book.author = document.getElementById('Author').value;
+  book.title = document.getElementById('Title').value;
   myLibrary.addBook(book);
   displayBooks();
   SaveLibrary();
 }
 
 function removeBook(id) {
-  myLibrary.removeBook();
+  myLibrary.removeBook(id);
   displayBooks();
   SaveLibrary();
 }
@@ -43,32 +43,24 @@ function removeBook(id) {
 function displayBooks() {
   const libraryDiv = document.getElementById('library');
   libraryDiv.innerHTML = '';
-  library.map((book) => {
+  myLibrary.library.map((book) => {
     const divBook = document.createElement('div');
-    const p = document.createElement('p');
-    p.innerHTML = book.author;
-    const p2 = document.createElement('p');
-    p2.innerHTML = book.title;
-    const hr = document.createElement('hr');
+    const p = document.createElement('p'); 
+    p.innerHTML = `"${book.title}" by ${book.author}`;
     const btn = document.createElement('button');
     btn.innerHTML = 'Remove';
     btn.addEventListener('click', () => {
       removeBook(book.id);
     });
-    divBook.appendChild(p);
-    divBook.appendChild(p2);
-    divBook.appendChild(hr);
-    divBook.appendChild(btn);
-    libraryDiv.appendChild(divBook);
-    return book;
-  });
+  
+  divBook.appendChild(p);
+  })
 }
 
 window.onload = function () {
-
   displayBooks();
 };
 
 function SaveLibrary() {
- saveLibrary();
+ myLibrary.saveLibrary();
 }
