e220b6b98648d31e008d021e0acda4f251192d34
Carlos-SSH <carlos.anezeh@me.com>
2021-08-13 10:24:38 -0500

    Add bootstrap to this project and fixed issues in html file.
    add style to footer.
    
    fixedissue in addBook function in app.js.

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 /* eslint no-use-before-define:["error",{"functions":false}] */
+
 class Library {
   constructor() {
     this.library = JSON.parse(localStorage.getItem('library') || '[]');
     this.id = 0;
   }
 
-  // eslint-disable-next-line no-unused-vars
-  addBook() {
+  addBook(book) {
     this.id += 1;
     book.id = this.id;
     this.library.push(book);
   }
 
   removeBook(id) {
-    this.library = library.filter((book) => book.id !== id);
+    this.library = this.library.filter((book) => book.id !== id);
   }
-
 
   saveLibrary() {
     localStorage.setItem('library', JSON.stringify(this.library));
@@ -24,6 +23,7 @@ class Library {
 
 const myLibrary = new Library();
 
+// eslint-disable-next-line no-unused-vars
 function addBook() {
   const book = {};
   book.author = document.getElementById('Author').value;
@@ -31,8 +31,6 @@ function addBook() {
   myLibrary.addBook(book);
   displayBooks();
   SaveLibrary();
-  document.getElementById('Author').value = '';
-  document.getElementById('Title').value = ''; 
 }
 
 function removeBook(id) {
@@ -44,42 +42,24 @@ function removeBook(id) {
 function displayBooks() {
   const libraryDiv = document.getElementById('library');
   libraryDiv.innerHTML = '';
-  let rowCount = 0;
-
   myLibrary.library.map((book) => {
-    const divListElm = document.createElement('div');
-    const divBtn = document.createElement('div');
     const divBook = document.createElement('div');
-
-
-    if (rowCount % 2 === 0) {
-      divListElm.classList.add('bg-secondary');
-    }
-    rowCount += 1;
-    
-    divListElm.classList.add('list-group-item');
-    divListElm.classList.add('list-group-item-action');
-    divListElm.classList.add('d-flex');
-    divListElm.classList.add('justify-content-between');
-
-    const bookText = document.createElement('h4'); 
-    bookText.innerHTML = `"${book.title}" by ${book.author}`;
-
-    const btn = document.createElement('button');
-    
+    const p = document.createElement('p'); // Author
+    p.innerHTML = book.author;
+    const p2 = document.createElement('p'); // Book
+    p2.innerHTML = book.title;
+    const hr = document.createElement('hr');
+    const btn = document.createElement('BUTTON');
     btn.innerHTML = 'Remove';
-    btn.classList.add('btn');
-    btn.classList.add('btn-danger');
     btn.addEventListener('click', () => {
       removeBook(book.id);
     });
-    
-    divListElm.appendChild(divBook);
-    divListElm.appendChild(divBtn);
-
-    divBook.appendChild(bookText);
-    divBtn.appendChild(btn);
-    libraryDiv.appendChild(divListElm);
+    divBook.appendChild(p);
+    divBook.appendChild(p2);
+    divBook.appendChild(hr);
+    divBook.appendChild(btn);
+    libraryDiv.appendChild(divBook);
+    return book;
   });
 }
 
@@ -88,5 +68,5 @@ window.onload = function () {
 };
 
 function SaveLibrary() {
- myLibrary.saveLibrary();
+  myLibrary.saveLibrary();
 }
