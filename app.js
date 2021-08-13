034211f6fd99601d430eaa0fe2ce9e23f834ed53
Carlos-SSH <carlos.anezeh@me.com>
2021-08-13 11:08:00 -0500

    Provide new functions.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint no-use-before-define:["error",{"functions":false}] */
 
 class Library {
@@ -31,6 +32,8 @@ function addBook() {
   myLibrary.addBook(book);
   displayBooks();
   SaveLibrary();
+  document.getElementById('Author').value = '';
+  document.getElementById('Title').value = '';
 }
 
 function removeBook(id) {
@@ -42,29 +45,54 @@ function removeBook(id) {
 function displayBooks() {
   const libraryDiv = document.getElementById('library');
   libraryDiv.innerHTML = '';
+  let rowCount = 0;
+
   myLibrary.library.map((book) => {
+    const divListElm = document.createElement('div');
+    const divBtn = document.createElement('div');
     const divBook = document.createElement('div');
-    const p = document.createElement('p'); // Author
-    p.innerHTML = book.author;
-    const p2 = document.createElement('p'); // Book
-    p2.innerHTML = book.title;
-    const hr = document.createElement('hr');
+
+    if (rowCount % 2 === 0) {
+      divListElm.classList.add('bg-secondary');
+    }
+    rowCount += 1;
+    divListElm.classList.add('list-group-item');
+    divListElm.classList.add('list-group-item-action');
+    divListElm.classList.add('d-flex');
+    divListElm.classList.add('justify-content-between');
+
+    const bookText = document.createElement('h4');
+    bookText.innerHTML = `"${book.title}" by ${book.author}`;
     const btn = document.createElement('BUTTON');
     btn.innerHTML = 'Remove';
+    btn.classList.add('btn');
+    btn.classList.add('btn-danger');
+
     btn.addEventListener('click', () => {
       removeBook(book.id);
     });
-    divBook.appendChild(p);
-    divBook.appendChild(p2);
-    divBook.appendChild(hr);
-    divBook.appendChild(btn);
-    libraryDiv.appendChild(divBook);
+
+    divListElm.appendChild(divBook);
+    divListElm.appendChild(divBtn);
+
+    divBook.appendChild(bookText);
+    divBtn.appendChild(btn);
+    libraryDiv.appendChild(divListElm);
+
     return book;
   });
 }
 
+function setDateTime() {
+  const currentDate = document.getElementById('currentDate');
+  // eslint-disable-next-line no-unused-vars
+  const { DateTime } = luxon;
+  currentDate.innerHTML = DateTime.now().toFormat('MMM dd yyyy, t');
+}
+
 window.onload = function () {
   displayBooks();
+  setDateTime();
 };
 
 function SaveLibrary() {
